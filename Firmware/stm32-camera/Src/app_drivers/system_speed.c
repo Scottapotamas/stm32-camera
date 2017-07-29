c7000ee13d0b4a2afe8a75b578755a832ee25dcd
Scott Rapson <scottapotamas@gmail.com>
2017-07-29 22:47:44 +0930

    Introduced system_speed drivers

---
+++ Firmware/stm32-camera/Src/app_drivers/system_speed.c
@@ -0,0 +1,122 @@
+/**
+ * @file      system_speed.h
+ *
+ * @brief     Control the system clock speed for power saving purposes.
+ *
+ * @author    Marco Hess <marcoh@applidyne.com.au>
+ *
+ * @copyright (c) 2017 Applidyne Pty. Ltd. - All rights reserved.
+ */
+
+/* ----- System Includes ---------------------------------------------------- */
+
+#include <string.h>
+
+/* ----- Local Includes ----------------------------------------------------- */
+
+#include "system_speed.h"
+#include "simple_state_machine.h"
+#include "hal_system_speed.h"
+
+/* ----- Private Types ------------------------------------------------------ */
+
+typedef enum
+{
+   SYSTEM_SPEED_STATE_LOW,
+   SYSTEM_SPEED_STATE_HIGH
+} SystemSpeedStates_t;
+
+typedef struct
+{
+    SystemSpeedStates_t previousState;
+    SystemSpeedStates_t currentState;
+    SystemSpeedStates_t nextState;
+    uint32_t            high_speed_requested;
+} SystemSpeedStateVars_t;
+
+/* ----- Private Data ------------------------------------------------------- */
+
+SystemSpeedStateVars_t system_speed_state_machine;
+SystemSpeedStateVars_t *me = &system_speed_state_machine;
+
+/* ----- Public Functions --------------------------------------------------- */
+
+/** Init the system clock to a default level */
+
+PUBLIC void
+system_speed_init( void )
+{
+    memset( &system_speed_state_machine, 0, sizeof(system_speed_state_machine) );
+
+    hal_system_speed_low();
+
+    STATE_INIT_INITIAL( SYSTEM_SPEED_STATE_LOW );
+}
+
+/* -------------------------------------------------------------------------- */
+
+/** Set the system clock speed to a different level */
+
+PUBLIC void
+system_speed_set( SystemSpeed_t speed )
+{
+    switch( me->currentState )
+    {
+       case SYSTEM_SPEED_STATE_LOW:
+           STATE_ENTRY_ACTION
+               me->high_speed_requested = 0;
+               hal_system_speed_low();
+           STATE_TRANSITION_TEST
+                if( speed == SYSTEM_SPEED_FULL )
+                {
+                    me->high_speed_requested++;
+                    STATE_NEXT( SYSTEM_SPEED_STATE_HIGH );
+                }
+            STATE_EXIT_ACTION
+            STATE_END
+            break;
+
+       case SYSTEM_SPEED_STATE_HIGH:
+           STATE_ENTRY_ACTION
+               hal_system_speed_high();
+            STATE_TRANSITION_TEST
+                if( speed == SYSTEM_SPEED_FULL )
+                {
+                    me->high_speed_requested++;
+                }
+                if( speed == SYSTEM_SPEED_LOW_POWER )
+                {
+                    if( --me->high_speed_requested == 0 )
+                    {
+                        STATE_NEXT( SYSTEM_SPEED_STATE_LOW );
+                    }
+                }
+            STATE_EXIT_ACTION
+            STATE_END
+            break;
+    }
+}
+
+
+/* -------------------------------------------------------------------------- */
+
+PUBLIC SystemSpeed_t
+system_speed_get( void )
+{
+    SystemSpeed_t speed = SYSTEM_SPEED_LOW_POWER;
+
+    switch( me->currentState )
+    {
+        case SYSTEM_SPEED_STATE_LOW:
+            speed = SYSTEM_SPEED_LOW_POWER;
+            break;
+
+        case SYSTEM_SPEED_STATE_HIGH:
+            speed = SYSTEM_SPEED_FULL;
+            break;
+    }
+    return speed;
+}
+
+/* ----- End ---------------------------------------------------------------- */
+
