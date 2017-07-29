fad2fd23e6f3b6ba9419c746a12326d615d9bfbc
Scott Rapson <scottapotamas@gmail.com>
2017-07-29 22:47:44 +0930

    Introduced system_speed drivers

---
+++ Firmware/stm32-camera/Src/app_drivers/system_speed.h
@@ -0,0 +1,56 @@
+/**
+ * @file      system_speed.h
+ *
+ * @brief     Control the system clock speed for power saving purposes.
+ *
+ * @author    Marco Hess <marcoh@applidyne.com.au>
+ *
+ * @copyright (c) 2017 Applidyne Pty. Ltd. - All rights reserved.
+ */
+
+#ifndef SYSTEM_SPEED_H
+#define SYSTEM_SPEED_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ----- System Includes ---------------------------------------------------- */
+
+
+/* ----- Local Includes ----------------------------------------------------- */
+
+#include <stdbool.h>
+
+/* ----- Public Functions --------------------------------------------------- */
+
+#include "global.h"
+
+typedef enum
+{
+    SYSTEM_SPEED_LOW_POWER = 0,
+    SYSTEM_SPEED_FULL
+} SystemSpeed_t;
+
+/* -------------------------------------------------------------------------- */
+
+PUBLIC void
+system_speed_init( void );
+
+/* -------------------------------------------------------------------------- */
+
+PUBLIC void
+system_speed_set( SystemSpeed_t speed );
+
+/* -------------------------------------------------------------------------- */
+
+PUBLIC SystemSpeed_t
+system_speed_get( void );
+
+/* ----- End ---------------------------------------------------------------- */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SYSTEM_SPEED_H */
