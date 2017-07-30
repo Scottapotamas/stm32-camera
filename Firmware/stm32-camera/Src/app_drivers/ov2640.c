1377b8d31b83bf94934de8eeddadf7e8b498d73e
Scott Rapson <scottapotamas@gmail.com>
2017-07-31 01:33:25 +0930

    Added ov2640 driver files (empty)

---
+++ Firmware/stm32-camera/Src/app_drivers/ov2640.c
@@ -0,0 +1,59 @@
+/**
+ * @file      ov2640.c
+ *
+ * @brief     ov2640 Camera Driver
+ *
+ */
+
+/* ----- System Includes ---------------------------------------------------- */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+
+/* ----- Local Includes ----------------------------------------------------- */
+
+#include "global.h"
+#include "qassert.h"
+#include "app_config.h"
+#include "log.h"
+#include "ov2640.h"
+#include "hal_camera.h"
+//#include "hal_i2c.h"
+//#include "hal_dcmi.h"
+//#include "hal_dma.h"
+
+/* -------------------------------------------------------------------------- */
+
+//DEFINE_THIS_FILE; /* Used for ASSERT checks to define __FILE__ only once */
+
+/* ----- Types ------------------------------------------------------------- */
+
+
+/* ------------------------ Variables --------------------------------------- */
+
+/* ------------------------ Declarations ------------------------------------ */
+
+
+/* ------------------------ Implementation ---------------------------------- */
+
+/** Init the camera driver */
+
+PUBLIC void
+ov2640_init( void )
+{
+
+
+}
+
+/* -------------------------------------------------------------------------- */
+
+/** Stop the camera driver */
+
+PUBLIC void
+ov2640_deinit( void )
+{
+
+}
+
+/* ----- End ---------------------------------------------------------------- */
