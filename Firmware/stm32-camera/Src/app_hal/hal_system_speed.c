222fb1c732c56496f8b3b283ca15f0ca12f1b8ce
Scott Rapson <scottapotamas@gmail.com>
2017-07-31 16:18:10 +0930

    Refactor cpu load functions

--- Firmware/stm32-camera/Src/app_hal/hal_system_speed.c
+++ Firmware/stm32-camera/Src/app_hal/hal_system_speed.c
@@ -18,28 +18,30 @@
 #include "hal_system_speed.h"
 #include "stm32f4xx_hal.h"
 
+
 /* ----- Private Types ------------------------------------------------------ */
 
 /* ----- Private Prototypes ------------------------------------------------- */
 
 /* ----- Private Data ------------------------------------------------------- */
 
-volatile uint32_t cc_when_sleeping;
-volatile uint32_t cc_when_woken = 0;
-volatile uint32_t cc_awake_time = 0;
-volatile uint32_t cc_asleep_time = 0;
+volatile uint32_t cc_when_sleeping;		//timestamp when we go to sleep
+volatile uint32_t cc_when_woken = 0; 	//timestamp when we wake up
+volatile uint32_t cc_awake_time = 0;	//duration of 'active'
+volatile uint32_t cc_asleep_time = 0;	//duration of 'sleep'
 
 /* ----- Public Functions --------------------------------------------------- */
 
 PUBLIC void
 hal_system_speed_init(  )
 {
+	//Enable the DWT timer
 	CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
 	DWT->CYCCNT = 0;
 	DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
-
-
 }
+
+/* -------------------------------------------------------------------------- */
 
 PUBLIC void
 hal_system_speed_sleep( void )
@@ -47,23 +49,19 @@ hal_system_speed_sleep( void )
 	CRITICAL_SECTION_VAR();
 	CRITICAL_SECTION_START();
 
-	/* Add to working time */
 	cc_awake_time += DWT->CYCCNT - cc_when_woken;
-
-	/* Save count cycle time */
 	cc_when_sleeping = DWT->CYCCNT;
 
-	/* Go to sleep mode */
+	//Go to sleep. Wake on interrupt.
     HAL_PWR_EnterSLEEPMode( PWR_MAINREGULATOR_ON, PWR_SLEEPENTRY_WFI );
 
-	/* Increase number of sleeping time in CPU cycles */
     cc_asleep_time += DWT->CYCCNT - cc_when_sleeping;
-
-	/* Save current time to get number of working CPU cycles */
 	cc_when_woken = DWT->CYCCNT;
 
 	CRITICAL_SECTION_END();
 }
+
+/* -------------------------------------------------------------------------- */
 
 //return a 0-100% value for CPU load
 PUBLIC float
