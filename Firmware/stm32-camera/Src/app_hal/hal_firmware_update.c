e64f66f299acd341106bd405e9df3499eb7a49c2
Scott Rapson <scottapotamas@gmail.com>
2017-08-01 01:08:48 +0930

    Added software firmware update function

--- Firmware/stm32-camera/Src/app_hal/hal_firmware_update.c
+++ Firmware/stm32-camera/Src/app_hal/hal_firmware_update.c
@@ -15,6 +15,7 @@
 #include "hal_delay.h"
 #include "stm32f4xx_hal.h"
 #include "app_times.h"
+#include "hal_systick.h"
 
 /* ----- Defines ------------------------------------------------------------ */
 
@@ -25,7 +26,7 @@
 /* ----- Public Functions --------------------------------------------------- */
 
 PUBLIC void
-hal_firmware_update()
+hal_firmware_update_hardware()
 {
     /* This ensure that the watch dog does not kick while reset. */
     hal_watchdog_refresh();
@@ -37,6 +38,42 @@ hal_firmware_update()
 
     /* Go straight to the CORE_CM4 inline function for a reset */
     NVIC_SystemReset();
+
+    /* NO RETURN */
+}
+
+PUBLIC void
+hal_firmware_update_software()
+{
+    /* This ensure that the watch dog does not kick while reset. */
+    hal_watchdog_refresh();
+
+	void (*SysMemBootJump)(void);
+
+	//Address of boot program in memory (only applies to F429, see AN2606 table 110).
+	volatile uint32_t addr = 0x1FFF0000;
+
+	//Disable RCC and set to defaults
+	HAL_RCC_DeInit();
+
+	//Disable systick and set to defaults
+	SysTick->CTRL = 0;
+	SysTick->LOAD = 0;
+	SysTick->VAL = 0;
+
+	__disable_irq();
+
+	//Remap memory to address 0x00000000 in address space.
+	__HAL_SYSCFG_REMAPMEMORY_SYSTEMFLASH();
+
+	// Set the jump location
+	SysMemBootJump = (void (*)(void)) (*((uint32_t *)(addr + 4)));
+
+	//Set the stack pointer
+	__set_MSP(*(uint32_t *)addr);
+
+    //aaand JUMP!
+	SysMemBootJump();
 
     /* NO RETURN */
 }
