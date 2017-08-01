13916c4987abd86992b8bdb87f3fb10d713fd26e
Scott Rapson <scottapotamas@gmail.com>
2017-08-01 13:56:40 +0930

    Fleshed out hal_camera with IO control. Fixed incorrect gpio mode for STROBE pin.

--- Firmware/stm32-camera/Src/app_hal/hal_camera.c
+++ Firmware/stm32-camera/Src/app_hal/hal_camera.c
@@ -11,24 +11,53 @@
 
 #include "hal_camera.h"
 #include "hal_gpio.h"
-#include "hal_power.h"
 
 /* ----- Defines ------------------------------------------------------------ */
 
 /* ----- Variables ---------------------------------------------------------- */
 
 /* -------------------------------------------------------------------------- */
-/* --- CAMERA INTERFACE                                                   --- */
+/* --- CAMERA HARDWARE CONTROL                                            --- */
 /* -------------------------------------------------------------------------- */
 
-/* Enable/disable power to the camera. */
+/* Enable/disable 1.3V and 2.5V regulators. */
 
 PUBLIC void
 hal_camera_power( bool on )
 {
 	hal_gpio_write_pin( _PWR_EN, on );
+//    hal_gpio_write_pin( _CAM_PWDN, on );
+	//TODO work out CAM_PWDN integration with regulator startup. Or split into different function
 }
 
+/* -------------------------------------------------------------------------- */
+
+/* Get the strobe state from the sensor */
+PUBLIC bool
+hal_camera_strobe( void )
+{
+    return hal_gpio_read_pin( _CAM_STROBE );
+}
+
+/* -------------------------------------------------------------------------- */
+
+/* Control the reset line */
+PUBLIC void
+hal_camera_reset( bool on )
+{
+    hal_gpio_write_pin( _CAM_RESET, on );
+    //TODO add reset timing and/or power cycling
+}
+
+/* -------------------------------------------------------------------------- */
+
+/* Control the sensor's snapshot mode */
+PUBLIC void
+hal_camera_snapshot( bool on )
+{
+    hal_gpio_write_pin( _CAM_FREX, on );
+    //TODO include snapshot modes which use the _CAM_EXPST pin
+}
 
 /* ----- End ---------------------------------------------------------------- */
 
